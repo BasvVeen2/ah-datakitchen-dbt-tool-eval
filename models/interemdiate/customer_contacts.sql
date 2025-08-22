7f87a2334922049c30aa7acda367df4dd9e26ab7
Philip Coomber <philip.coomber@ah.nl>
2025-08-22 13:21:28 +0000

    add customer_contacts

---
+++ models/interemdiate/customer_contacts.sql
@@ -0,0 +1,9 @@
+{{ config(materialized="view", schema="intermediate", file_format="delta") }}
+
+select
+    customer_key,
+    contact.type as contact_type,
+    contact.value as contact_value,
+    contact.is_primary as is_primary_contact_type
+from {{ ref("customer") }}
+lateral view explode(contact_methods) contact_table as contact
