4011348bf8dae3eeeaaad594123822bf9ecae669
abbas.tikiwala@ah.nl <abbas.tikiwala@ah.nl>
2025-08-27 11:50:31 +0000

    updates

--- abbas_tikiwala/models/intermediate/customer_order.sql
+++ abbas_tikiwala/models/intermediate/customer_order.sql
@@ -6,22 +6,22 @@
 
 with customer_orders as (
     select
-        customer_key,
+        customer.customer_key,
         count(order_key) as total_orders,
-        sum(ordertotal_price_amount) as total_order_amount,
+        sum(total_price) as total_order_amount,
         avg(total_price) as average_order_amount
     from
         {{ ref('customer') }} as customer
     inner join
         {{ ref('order') }} as orders
     on
-        customer.customer_id = orders.customer_key
+        customer.customer_key = orders.customer_key
     group by
-        customer_key
+        customer.customer_key
 )
 
 select
-    customer_id,
+    customer_key,
     total_orders,
     total_order_amount,
     average_order_amount
