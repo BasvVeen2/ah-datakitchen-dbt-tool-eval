0430a9040d6fea4b306ea19661d8b686d9b63a02
Bas van Veen <Bas.van.Veen2@ah.nl>
2025-09-02 09:11:41 +0000

    chore/thisisit

--- datakitchen_test/models/intermediate/order_lineitems.sql
+++ datakitchen_test/models/intermediate/order_lineitems.sql
@@ -2,67 +2,39 @@
     config(
         materialized="incremental",
         partition_by = ['order_date'],
+        unique_key=['order_key','linenumber']
         schema="intermediate",
         tblproperties = {'delta.enableChangeDataFeed': 'true'},
         file_format="delta"
         ) 
 }}
 
+SELECT
+    o.order_key,
+    o.customer_key,
+    o.order_date,
+    o.total_price,
+    o.order_status,
+    -- Unnesting line items array
+    line_item.part_key,
+    line_item.supplier_key,
+    line_item.linenumber,
+    line_item.quantity,
+    line_item.extended_price,
+    line_item.discount,
+    line_item.tax,
+    line_item.line_status,
+    line_item.ship_date,
+    line_item.shipment_details.mode as ship_mode,
+    line_item.shipment_details.receipt_date,
+    line_item.shipment_details.transit_days,
+    -- Calculate derived fields
+    CAST(line_item.extended_price * (1 - line_item.discount) AS decimal(15,2)) as discounted_price,
+    CAST(line_item.extended_price * (1 - line_item.discount) * (1 + line_item.tax) AS decimal(15,2)) as final_price,
+    current_timestamp() as last_modified
+FROM {{ ref('order') }} o
+LATERAL VIEW EXPLODE(line_items) line_items_table AS line_item
+
 {% if is_incremental()%}
-INSERT INTO TABLE alh_nonprd_soubhik.dbt_datakitchen_prd_intermediate.order_lineitems
-    REPLACE USING (order_date)
-    SELECT
-        o.order_key,
-        o.customer_key,
-        o.order_date,
-        o.total_price,
-        o.order_status,
-        -- Unnesting line items array
-        line_item.part_key,
-        line_item.supplier_key,
-        line_item.linenumber,
-        line_item.quantity,
-        line_item.extended_price,
-        line_item.discount,
-        line_item.tax,
-        line_item.line_status,
-        line_item.ship_date,
-        line_item.shipment_details.mode as ship_mode,
-        line_item.shipment_details.receipt_date,
-        line_item.shipment_details.transit_days,
-        -- Calculate derived fields
-        CAST(line_item.extended_price * (1 - line_item.discount) AS decimal(15,2)) as discounted_price,
-        CAST(line_item.extended_price * (1 - line_item.discount) * (1 + line_item.tax) AS decimal(15,2)) as final_price,
-        current_timestamp() as last_modified
-    FROM {{ ref('order') }} o
-    LATERAL VIEW EXPLODE(line_items) line_items_table AS line_item
     WHERE o.last_modified > (select max(last_modified) from {{ this }})
-
-{% else %}
-    SELECT
-        o.order_key,
-        o.customer_key,
-        o.order_date,
-        o.total_price,
-        o.order_status,
-        -- Unnesting line items array
-        line_item.part_key,
-        line_item.supplier_key,
-        line_item.linenumber,
-        line_item.quantity,
-        line_item.extended_price,
-        line_item.discount,
-        line_item.tax,
-        line_item.line_status,
-        line_item.ship_date,
-        line_item.shipment_details.mode as ship_mode,
-        line_item.shipment_details.receipt_date,
-        line_item.shipment_details.transit_days,
-        -- Calculate derived fields
-        CAST(line_item.extended_price * (1 - line_item.discount) AS decimal(15,2)) as discounted_price,
-        CAST(line_item.extended_price * (1 - line_item.discount) * (1 + line_item.tax) AS decimal(15,2)) as final_price,
-        current_timestamp() as last_modified
-    FROM {{ ref('order') }} o
-    LATERAL VIEW EXPLODE(line_items) line_items_table AS line_item
-
 {% endif %}
