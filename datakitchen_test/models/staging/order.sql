49585d83175b59144a96e9a5d16565f075415f59
Bas van Veen <Bas.van.Veen2@ah.nl>
2025-09-02 12:52:57 +0000

    ugh

--- datakitchen_test/models/staging/order.sql
+++ datakitchen_test/models/staging/order.sql
@@ -8,23 +8,40 @@
     tblproperties = {'delta.enableChangeDataFeed': 'true'}
 ) }}
 
-
+WITH ranked_orders AS (
+    SELECT
+        o_orderkey as order_key,
+        o_custkey as customer_key,
+        o_orderstatus as order_status,
+        o_totalprice as total_price,
+        o_orderdate as order_date,
+        o_orderpriority as order_priority,
+        o_clerk as clerk,
+        o_shippriority as ship_priority,
+        o_comment as comment,
+        shipping_info,
+        line_items,
+        current_timestamp() as last_modified,
+        ROW_NUMBER() OVER (PARTITION BY o_orderkey ORDER BY o_orderdate desc) AS row_num
+    FROM {{source("tpch", "order")}}
+    {% if is_incremental() %}
+        where o_orderdate > (select max(last_modified) from {{ this }})
+    {% endif %}
+)
 SELECT
-    o_orderkey as order_key,
-    o_custkey as customer_key,
-    o_orderstatus as order_status,
-    o_totalprice as total_price,
-    o_orderdate as order_date,
-    o_orderpriority as order_priority,
-    o_clerk as clerk,
-    o_shippriority as ship_priority,
-    o_comment as comment,
+    order_key,
+    customer_key,
+    order_status,
+    total_price,
+    order_date,
+    order_priority,
+    clerk,
+    ship_priority,
+    comment,
     shipping_info,
     line_items,
-    current_timestamp() as last_modified
-    
-FROM {{source("tpch", "order")}}
+    last_modified
+FROM ranked_orders
+WHERE row_num = 1;
 
-{% if is_incremental() %}
-  where o_orderdate > (select max(last_modified) from {{ this }})
-{% endif %}
+
