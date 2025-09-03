a4f34aad5985cd26d830eef092b1f4eb195ffd0e
Bas van Veen <Bas.van.Veen2@ah.nl>
2025-09-03 12:03:48 +0000

    add

--- datakitchen_test/models/staging/region.sql
+++ datakitchen_test/models/staging/region.sql
@@ -6,7 +6,6 @@
         target_alias='t',
         source_alias='s',
         matched_condition='(t.region_name <> s.region_name OR t.r_comment <> s.r_comment)',
-
         schema="staging",
         file_format='delta',
         tblproperties = {'delta.enableChangeDataFeed': 'true'}
